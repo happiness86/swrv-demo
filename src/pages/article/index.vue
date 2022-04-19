e073d76e35a9bbb849dd6cc52bb629a76dcb571d
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2022-04-19 14:45:13 +0800

    feat: add mutate

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -3,17 +3,33 @@ import useSWRV from 'swrv'
 import Count from '~/components/Count.vue'
 const fetchFactory = (url: string) => fetch(url).then(r => r.json())
 
-const { data, error } = useSWRV('/api/articles', fetchFactory)
+const { data, error, mutate: mutateArticleList } = useSWRV('/api/articles', fetchFactory)
 
-const showCount = ref(false)
+const showCount = ref(true)
 setTimeout(() => showCount.value = true, 3000)
+
+const updateArticleList = async() => {
+  await fetch('/api/articles', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title: `Untitled article ${Math.ceil(Math.random() * 100)}`,
+      author: 'Andy',
+      link: 'https://www.vuemastery.com/blog/part-3-client-side-graphql-with-vuejs',
+    }),
+  })
+  mutateArticleList()
+}
 </script>
 <template>
   <div v-if="error">
     something got wrong
   </div>
-  <div v-for="item in data" :key="item.id">
-    <a :href="item.link">{{ item.title }}</a>
+  <div v-for="(item, index) in data" :key="item.id">
+    {{ index }}: <a :href="item.link">{{ item.title }}</a>
   </div>
   <Count v-if="showCount" />
+  <button block border-1 p-2 m-auto @click="updateArticleList">
+    update article list
+  </button>
 </template>
